81810dc5fd201e465dcdf5422774f1beac0af35a
tellydelly <martinwilson450@gmail.com>
2022-06-20 20:47:33 -0700

    started description turncation

---
+++ portfolio/src/Components/ProjectCard/ProjectCard.jsx
@@ -0,0 +1,32 @@
+import './ProjectCard.css'
+import React, {useState} from 'react'
+import { Carousel, CarouselItem,Button } from 'react-bootstrap'
+
+const ProjectCard = ({projectData}) => {
+    // const [stringMax, setStringMax] = useState(5);
+    // const description = create
+  
+    // if(description.length > stringMax){
+    //     setDescription(description.slice(0,stringMax) + '...')
+    // } 
+
+    return (
+    <div className='project-card'>
+        <h1>{projectData.title}</h1>
+        <p></p>
+        <Carousel interval={3000}>
+            {projectData.imgList  && projectData.imgList.map((image) => {
+                <CarouselItem>
+                    <img alt={image.altText} src={image.src}/>
+                </CarouselItem>
+            })}
+        </Carousel>
+        <div className='project-card-button-group'>
+        <Button className='header-button' href="https://github.com/TelleDelly">Project Repo</Button>
+        <Button className='header-button' href='https://www.linkedin.com/in/martin-wilson-tel/'>Live Site</Button>
+        </div>
+    </div>
+  )
+}
+
+export default ProjectCard
